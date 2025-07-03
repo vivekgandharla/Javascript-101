88a4cbfe0ee383a4cfcd85f6ff77e9b9da2e904d
vivekgandharla <sonugandherla01@gmail.com>
2025-07-03 10:34:11 +0530

    Basic stuff

--- javacsript soluttions/solutions.js
+++ javacsript soluttions/solutions.js
@@ -1,1 +1,22 @@
 console.log("Hello, World!");
+
+//question 1
+for(let i = 1; i <=10; i++){
+    console.log(i);
+}
+//question 2
+console.log(1+1);
+console.log(1+200);
+console.log(200+"300");
+console.log("learning"+"javascript"+400);
+
+//question 3
+console.log(2===2);
+console.log(2=="2");
+console.log(2===+"2");
+console.log("learnJavascript"==="learnJavascript");
+console.log("learnJavascript"==="LearnJavascript");
+console.log(0.3 == 0.30);
+console.log(1.0 == 1);
+console.log(1.0 === 1);
+
